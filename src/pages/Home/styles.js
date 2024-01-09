5af873bf566edf7fd1badebaeffe6dc7479f6ceb
EduViera131 <eduviera369@gmail.com>
2024-01-09 13:46:30 -0300

    :fix: home page main structure change

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100vw;
-  
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 1fr 10fr 1fr;
   gap: 1.5rem;
 `
 
